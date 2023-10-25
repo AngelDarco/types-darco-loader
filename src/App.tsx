1b5cec732aafa3899885e2f1e8a5b2247cf22253
Angel Darco <angel92-eme@hotmail.com>
2023-10-25 00:53:38 -0500

    :sparkles: routes updated with new pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App w-full h-screen">
+    <div className="App w-full h-screen flex items-end justify-center">
       <Header />
       <Routes />
     </div>
--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,18 +1,21 @@
 import { Route } from "wouter";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
+import Offers from "./pages/offers/Offers";
+import Price from "./pages/price/Price";
+import Clients from "./pages/clients/Clients";
+import Contact from "./pages/contact/Contact";
+import Car from "./pages/car/Car";
 const Routes = () => {
   return (
     <>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/projects">
-        <Home />
-      </Route>
+    <Route path="/" component={Home} />
+    <Route path="/offers" component={Offers} />
+    <Route path="/prices" component={Price} />
+    <Route path="/about" component={About} />
+    <Route path="/clients" component={Clients} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/car" component={Car} />
     </>
   );
 };
