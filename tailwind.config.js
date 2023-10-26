ab6d000983f773899784235f7e464a5c44b72f1b
Angel Darco <angel92-eme@hotmail.com>
2023-10-26 10:55:02 -0500

    :sparkles: custom keyfarmes added to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
+  plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loader1": "loader1 3s ease-in-out infinite",
+        "loader2": "loader2 3s ease-in-out infinite",
+      },
+      keyframes: {
+        loader1: {
+          "0%": { width: "0%", boxShadow: "0 0 6px var(--color)" },
+          "40%": { width: "100%" },
+          "100%": { width: "100%" }
+        },
+        loader2: {
+          "30%": { width: "0%", boxShadow: "0 0 6px var(--color)" },
+          "50%": { width: "100%" },
+          "100%": { width: "0%" }
+        }
+      },
+    },
     screens: {
       "laptop": { max: "900px" },
     },
   },
-  darkMode: "class",
-  plugins: [],
 };
 
