36cf8fa60ab80100113b5dd021a080298497b4bb
Angel Darco <angel92-eme@hotmail.com>
2023-11-05 12:17:11 -0500

    :hammer: new tailwind config to include bg-radiant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,32 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   plugins: [],
   theme: {
     extend: {
       animation: {
-        "loader1": "loader1 5s ease-in-out infinite",
-        "loader2": "loader2 5s ease-in-out infinite",
+        loader1: "loader1 5s ease-in-out infinite",
+        loader2: "loader2 5s ease-in-out infinite",
       },
       keyframes: {
         loader1: {
           "0%": { width: "0%" },
           "40%": { width: "100%" },
-          "100%": { width: "100%" }
+          "100%": { width: "100%" },
         },
         loader2: {
           "30%": { width: "0%" },
           "50%": { width: "100%" },
-          "100%": { width: "0%" }
-        }
+          "100%": { width: "0%" },
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
     },
     screens: {
-      "laptop": { max: "900px" },
+      laptop: { max: "900px" },
     },
   },
 };
-
